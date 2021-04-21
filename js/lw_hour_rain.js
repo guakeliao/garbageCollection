76348fb4b12b68764a0ebb71e734f0b7de82c0d4
liaowei <liaowei3@asiainfo.com>
2021-04-21 17:12:29 +0800

    6点的红包雨
    6点的红包雨

--- js/lw_hour_rain.js
+++ js/lw_hour_rain.js
@@ -6,7 +6,7 @@
 1,30 8-23 * 4 * https://raw.githubusercontent.com/guakeliao/garbageCollection/master/js/lw_hour_rain.js  tag=整点京豆雨, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_syj.png, enabled=true
 */
 const $ = new Env('整点京豆雨');
-let redrainId = "RRAAYPHu6s5pWzYMqmfNygvvgJTKdq";
+let redrainId = "RRA4Kr8hch51J49qasBvEv8Agf8chAD";
 let allMessage = '';
 let bodyList = {
   '20': {
