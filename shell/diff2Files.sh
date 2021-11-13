471fb50c68c6d520bc3616f45780393848e6c5e3
guakeliao <598951856@qq.com>
2021-11-13 11:28:49 +0800

    添加shell脚本的日志

--- shell/diff2Files.sh
+++ shell/diff2Files.sh
@@ -32,26 +32,30 @@ function diff2Files() {
   local data2=$(wc -c < $path2)
   if [ $data1 != $data2 ]; then
       #不相等,迁移覆盖
-      echo "not equal"
       cp -f $path1 $path2
   fi
 }
 
 
 function diff2dir() {
+    starttime=$(date +%Y-%m-%d\ %H:%M:%S)
+    echo $starttime
     local filePaths1 filePaths2 fileName1 fileName2
     filePaths1=$(scandir $1)
-    filePaths2=$(scandir $2)
+    filePaths2=$(scandir $2)    
     for path1 in ${filePaths1}; do
+        echo "================="
+        echo $path1
         fileName1=${path1##*/}
-        echo ${fileName1}
         for path2 in ${filePaths2}; do
             fileName2=${path2##*/}
-            echo ${fileName2}
             if [ $fileName1 = $fileName2 ]; then
+                echo $path2
                 diff2Files $path1 $path2
             fi
         done
     done
+    endtime=$(date +%Y-%m-%d\ %H:%M:%S)
+    echo $endtime
 }
 diff2dir $1 $2
