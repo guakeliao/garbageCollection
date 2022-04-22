ea6038daad7b1ee9e8d23d5491f2645e6f3f7771
liaowei <guakeliao@gmail.com>
2022-04-22 12:29:53 +0800

    fixbug

--- lw_write.js
+++ lw_write.js
@@ -37,7 +37,10 @@ function dealCodes(remoteCodes, filePath, user = null) {
     let localCodes = readTyt(filePath);
     //保证远端的code放到最前面。便于后面的去重
     codes = codes.concat(remoteCodes);
-    if (Array.isArray(localCodes)) {
+    if (Array.isArray(localCodes) && localCodes.length > 0) {
+        //让localCodes的第一个元素保留到前面
+        let firstCode = localCodes.splice(0, 1)[0];
+        codes.splice(0, 0, firstCode)
         codes = codes.concat(localCodes);
     }
     //codes去重
@@ -70,11 +73,21 @@ function dealCodes(remoteCodes, filePath, user = null) {
 }
 
 // const FILE = './tyt.json'
-// let ss = [{
-//     "user": "669", "packetId": "1111", "ok": false
-// }, {
-//     "user": "669", "packetId": "2222", "ok": false
-// }]
-// dealCodes(ss, FILE, '挂科廖')
+// let ss = [  {
+//     "user": "婷婷玉立789",
+//     "packetId": "55a9e29ad6e34aec81858e78dfd2d52f-66656565",
+//     "ok": false
+// },
+//     {
+//         "user": "wdiXmBKQrKBGZQ99",
+//         "packetId": "ee4b8aacc2b94e95897b0e727709b734-d2RpWG1CS1FyS0JHWlE!",
+//         "ok": false
+//     },
+//     {
+//         "user": "jd_YhpNXRevsGBY999",
+//         "packetId": "78c9c488c17b4bb68e15b67d9a388eca-amRfWWhwTlhSZXZzR0JZ",
+//         "ok": false
+//     }]
+// dealCodes(ss, FILE, '婷婷玉立789')
 
 module.exports.dealCodes = dealCodes;
